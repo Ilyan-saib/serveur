872095c979ee099edd82381a89aef349061b4f8c
CannelleJP <isaibmezghiche@edenschool.fr>
2022-11-08 17:10:05 +0100

    lack pas pelo

--- api.js
+++ api.js
@@ -1,12 +1,28 @@
 import express from 'express'
+import cors from 'cors'
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res)=> {
-    res.send("hello world")
+app.use(cors({
+    origin: '*'
+}))
+
+
+
+    app.get('/', (req, res)=> {
+       
+        setTimeout(()=>{
+
+            res.json({id: 1, name: "bob", age: 69})
+        }, 3000)
+    
 })
+
+
 
 app.listen(port , () => {
     console.log(`Example app listening on port ${port}`);
 })
+
 
--- js/main.js
+++ js/main.js
@@ -2,37 +2,53 @@ const httpRequest = new XMLHttpRequest();
 
 
 function reqListener(event) {
-  console.log('onload:',event)
+  console.log('onload:', event)
   console.log(httpRequest.response);
 }
 
 httpRequest.onload = reqListener;
 
 httpRequest.onerror = (event) => {
-  console.log('onerror:' , event);
+  console.log('onerror:', event);
 }
 httpRequest.onloadend = (event) => {
-  console.log('onloadend',event);
+  console.log('onloadend', event);
 }
 
 httpRequest.onloadstart = (event) => {
-  console.log('onloadstart',event);
+  console.log('onloadstart', event);
 }
 
 httpRequest.onloadprogress = (event) => {
-  console.log('onloadprogress',event);
+  console.log('onloadprogress', event);
 }
 
+httpRequest.onabort = (event) => {
+  console.log("abord", event);
+}
 
 httpRequest.onreadystatechange = (event) => {
-    console.log('onreadystatechange' , event)
+  console.log('onreadystatechange', event)
 }
 
 httpRequest.onreadytimeout = (event) => {
-  console.log('onreadytimeout' , event)
+  console.log('onreadytimeout', event)
 }
 
+const bttn = document.getElementById('jajaja')
+bttn.addEventListener('click', () => {
+  httpRequest.open('GET', 'http://127.0.0.1:3000', true)
+  httpRequest.send()
+})
+
+const btn = document.getElementById('ja')
+btn.addEventListener('click', () => {
+  httpRequest.abort()
+})
 
 
-httpRequest.open('GET','../text.txt' , true)
-httpRequest.send()
+
+
+
+
+
